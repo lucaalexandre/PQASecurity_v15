8d4a4035e038bdd24ea488eef65b77036debdad0
Luca <lucaalexandrepp@gmail.com>
2019-08-27 10:45:53 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pqasecurityv15/splashscreen.java
@@ -0,0 +1,29 @@
+package com.example.pqasecurityv15;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstaceState){
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.splashscreen);
+
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 5000);
+
+    }
+}
