53e08121cba8529146cebb58865dda5a2ba8cff6
Luca <lucaalexandrepp@gmail.com>
2019-08-28 22:05:08 -0300

    Criação da tela de fingerprint

--- app/src/main/java/com/example/pqasecurityv15/MainActivity.java
+++ app/src/main/java/com/example/pqasecurityv15/MainActivity.java
@@ -1,31 +1,33 @@
 package com.example.pqasecurityv15;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_digital = (Button) findViewById(R.id.btn_digital);
-
-        btn_digital.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.btn_digital);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it = new Intent(MainActivity.this, fingerprintActivity.class);
-                startActivity(it);
+                Intent intent = new Intent(MainActivity.this, fingerprintActivity.class);
+                startActivity(intent);
             }
         });
 
     }
+
 
 
 }
